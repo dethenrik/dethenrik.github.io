613685c492d750821e63a43a5e7689b9097b4fcc
dethenrik <almand1999@hotmail.com>
2023-10-18 13:22:04 +0200

    cc

--- src/app/beviser/beviser.component.ts
+++ src/app/beviser/beviser.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-declare function OpenImage():void; {
-}
+import { Component, ElementRef, HostListener } from '@angular/core';
 @Component({
   selector: 'app-beviser',
   templateUrl: './beviser.component.html',
   styleUrls: ['./beviser.component.css']
 })
 export class BeviserComponent {
-  ngoninit():void {
-    OpenImage();
+  constructor(private el: ElementRef){}
+
+  @HostListener('click')
+  imagechange(){
+    var src:any = this.el.nativeElement.src;
+    var prev:any = document.getElementById("preview");
+    prev.src = src;
+    var imageSlide = document.getElementsByClassName("img-slide");
+    for(let i=0; i<imageSlide.length; i++){
+      imageSlide[i].classList.remove("active");
+    }
+    this.el.nativeElement.parentElement.classList.add("active")
   }
 }
