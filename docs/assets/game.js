a93a132b8d591692bd3eae9412619427d0d5b08f
dethenrik <almand1999@hotmail.com>
2023-10-26 09:56:48 +0200

    added dropdown menu so i am able to pick animation

--- docs/assets/game.js
+++ docs/assets/game.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function Game() {
+
+    let playerState = 'idle';
+    const dropdown = document.getElementById('animations');
+    dropdown.addEventListener('change', function(e){
+        playerState = e.target.value;
+    });
+
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     
@@ -10,7 +17,8 @@ document.addEventListener('DOMContentLoaded', function Game() {
 
     const spriteWidth = 575;
     const spriteHeight = 523;
-    
+
+
     //let frameX = 0;
     //let frameY = 1;     // this is the absolute position of the sprite
 
@@ -27,6 +35,38 @@ document.addEventListener('DOMContentLoaded', function Game() {
         {
             name: 'jump',
             frames: '7',
+        },
+        {
+            name: 'fall',
+            frames: '7',
+        },
+        {
+            name: 'run',
+            frames: '9',
+        },
+        {
+            name: 'dizzy',
+            frames: '11',
+        },
+        {
+            name: 'sit',
+            frames: '5',
+        },
+        {
+            name: 'roll',
+            frames: '7',
+        },
+        {
+            name: 'bite',
+            frames: '7',
+        },
+        {
+            name: 'ko',
+            frames: '12',
+        },
+        {
+            name: 'getHit',
+            frames: '4',
         }
     ];
     animationStates.forEach((state, index) => {
@@ -44,10 +84,10 @@ document.addEventListener('DOMContentLoaded', function Game() {
 
     function animate() {
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['idle'].loc.length;//this is the relative position of the sprite
+        let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;//this is the relative position of the sprite
         let frameX = spriteWidth * position;
-        let frameY = spriteAnimations['idle'].loc[position].y;
-        ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+        let frameY = spriteAnimations[playerState].loc[position].y;
+        ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
         gameFrame++;
         requestAnimationFrame(animate);
     }
